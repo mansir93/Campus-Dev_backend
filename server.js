43d1ad885cbd1db917653563dfcc7a8462c67993
Mansir <mansiraziz93@gmail.com>
2023-08-16 00:33:12 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/auth");
 
 const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css";
 
 const PORT = process.env.PORT || 5001;
 connectDB();
