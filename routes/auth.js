59e7998158b40c508edd00c536e004895119d4a3
Mansir <mansiraziz93@gmail.com>
2023-08-16 00:21:01 +0000

    added docs

--- routes/auth.js
+++ routes/auth.js
@@ -1,73 +1,112 @@
 const express = require("express");
-const { register,login } = require("../controllers/authController");
+const { register, login } = require("../controllers/authController");
 
 const router = require("express").Router();
-/**
+
+/** 
  * @swagger
- * components:
- *   schemas:
- *     Book:
- *       type: object
- *       required:
- *         - title
- *         - author
- *         - finished
- *       properties:
- *         id:
- *           type: string
- *           description: The auto-generated id of the book
- *         title:
- *           type: string
- *           description: The title of your book
- *         author:
- *           type: string
- *           description: The book author
- *         finished:
- *           type: boolean
- *           description: Whether you have finished reading the book
- *         createdAt:
- *           type: string
- *           format: date
- *           description: The date the book was added
- *       example:
- *         id: d5fE_asz
- *         title: The New Turing Omnibus
- *         author: Alexander K. Dewdney
- *         finished: false
- *         createdAt: 2020-03-10T04:05:06.157Z
+ * 
+* components:
+*   schemas:
+*     User:
+*       type: object
+*       properties:
+*         username:
+*           type: string
+*           minLength: 3
+*           maxLength: 20
+*         email:
+*           type: string
+*           format: email
+*           maxLength: 50
+*         password:
+*           type: string
+*           minLength: 6
+*         profile_pic:
+*           type: string
+*         cover_pic:
+*           type: string
+*         followers:
+*           type: array
+*           items:
+*             type: string
+*         following:
+*           type: array
+*           items:
+*             type: string
+*         isAdmin:
+*           type: boolean
+
  */
 
 /**
  * @swagger
- * tags:
- *   name: register
- *   description: Auth APIsI
- * /register:
- *   post:
- *     summary: register a new user
- *     tags: [register]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Book'
- *     responses:
- *       200:
- *         description: register successfully.
+ *  tags:
+ *   name: Authentication
+ *   description: 
+
+* /register:
+*   post:
+*     summary: Register a new user
+*     tags:
+*       - Authentication
+*     requestBody:
+*       description: User registration details
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               username:
+*                 type: string
+*                 minLength: 3
+*                 maxLength: 20
+*               email:
+*                 type: string
+*                 format: email
+*                 maxLength: 50
+*               password:
+*                 type: string
+*                 minLength: 6
+*     responses:
+*       '201':
+*         description: User successfully registered
+*       '400':
+*         description: Invalid input or registration failed
+*
+*
+ */
+router.post("/register", register);
+
+/**
+ *  @swagger
+ * /login:
+ *     post:
+ *       summary: User login
+ *       tags:
+ *         - Authentication
+ *       requestBody:
+ *         description: User login details
+ *         required: true
  *         content:
  *           application/json:
  *             schema:
- *             $ref: '#/components/schemas/Book'
- *       500:
- *         description: Some server error
+ *               type: object
+ *               properties:
+ *                 email:
+ *                   type: string
+ *                   format: email
+ *                 password:
+ *                   type: string
+ *                   minLength: 6
+ *       responses:
+ *         '200':
+ *           description: Login successful
+ *         '401':
+ *           description: Unauthorized or invalid credentials
  *
  */
-
-router.post("/register", register);
-
-
-
 router.post("/login", login);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,32 +3,6 @@ const express = require("express");
 const router = require("express").Router();
 
 
-/**
- * @swagger
- * tags:
- *   name: register
- *   description: Auth APIsI
- * /:
- *   post:
- *     summary: register a new user
- *     tags: [register]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Book'
- *     responses:
- *       200:
- *         description: register successfully.
- *         content:
- *           application/json:
- *             schema:
- *             $ref: '#/components/schemas/Book'
- *       500:
- *         description: Some server error
- *
- */
 
 
 
