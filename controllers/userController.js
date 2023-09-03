c9ab249d9e2afb001919880371e4a5435155c9c4
Mansir <mansiraziz93@gmail.com>
2023-09-03 16:14:14 +0000

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,6 +38,7 @@ exports.deleteUser = asyncHandler(async (req, res) => {
     return res.status(403).json("You can delete only your account!");
   }
 });
+
 //get a user
 exports.getSingleUser = asyncHandler(async (req, res) => {
   try {
@@ -48,43 +49,62 @@ exports.getSingleUser = asyncHandler(async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 //follow a user
 exports.followUser = asyncHandler(async (req, res) => {
-  if (req.body.userId !== req.params.id) {
-    try {
-      const user = await User.findById(req.params.id);
-      const currentUser = await User.findById(req.body.userId);
-      if (!user.followers.includes(req.body.userId)) {
-        await user.updateOne({ $push: { followers: req.body.userId } });
-        await currentUser.updateOne({ $push: { followings: req.params.id } });
-        res.status(200).json("user has been followed");
-      } else {
-        res.status(403).json("you allready follow this user");
-      }
-    } catch (err) {
-      res.status(500).json(err);
+  try {
+    // Validate input and permissions
+    if (req.body.userId === req.params.id) {
+      return res.status(403).json("You can't follow yourself");
     }
-  } else {
-    res.status(403).json("you cant follow yourself");
+    const userToFollow = await User.findById(req.params.id);
+    const currentUser = await User.findById(req.body.userId);
+
+    if (!userToFollow || !currentUser) {
+      return res.status(404).json("User not found");
+    }
+
+    if (userToFollow.followers.includes(req.body.userId)) {
+      return res.status(403).json("You already follow this user");
+    }
+    // Update the user documents
+    await userToFollow.updateOne({ $push: { followers: req.body.userId } });
+    await currentUser.updateOne({ $push: { followings: req.params.id } });
+
+    res.status(200).json("User has been followed");
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Internal server error");
   }
 });
+
+
 //unfollow a user
 exports.unfollowUser = asyncHandler(async (req, res) => {
-  if (req.body.userId !== req.params.id) {
-    try {
-      const user = await User.findById(req.params.id);
-      const currentUser = await User.findById(req.body.userId);
-      if (user.followers.includes(req.body.userId)) {
-        await user.updateOne({ $pull: { followers: req.body.userId } });
-        await currentUser.updateOne({ $pull: { followings: req.params.id } });
-        res.status(200).json("user has been unfollowed");
-      } else {
-        res.status(403).json("you dont follow this user");
-      }
-    } catch (err) {
-      res.status(500).json(err);
+  try {
+    // Validate input and permissions
+    if (req.body.userId === req.params.id) {
+      return res.status(403).json("You can't unfollow yourself");
     }
-  } else {
-    res.status(403).json("you cant unfollow yourself");
+
+    const userToUnfollow = await User.findById(req.params.id);
+    const currentUser = await User.findById(req.body.userId);
+
+    if (!userToUnfollow || !currentUser) {
+      return res.status(404).json("User not found");
+    }
+
+    if (!userToUnfollow.followers.includes(req.body.userId)) {
+      return res.status(403).json("You don't follow this user");
+    }
+
+    // Update the user documents to unfollow
+    await userToUnfollow.updateOne({ $pull: { followers: req.body.userId } });
+    await currentUser.updateOne({ $pull: { followings: req.params.id } });
+
+    res.status(200).json("User has been unfollowed");
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Internal server error");
   }
 });
